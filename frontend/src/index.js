f5d10b0adee656ecee69ee7b0330532e5e39b72b
felipe <felipe_matheus.p@hotmail.com>
2023-10-03 16:04:22 -0300

    removing strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,11 +10,14 @@ import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  // </React.StrictMode> 
+  <React>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
