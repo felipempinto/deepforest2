2eb802996b6d9d600021ff512911ebdca068750d
felipe <felipe_matheus.p@hotmail.com>
2023-10-03 15:37:30 -0300

    Adding static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,6 +188,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, "build/static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
     # if DEBUG:
     #     STATIC_URL = '/static/'
     #     STATIC_ROOT = 'staticfiles'
